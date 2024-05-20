16c1261454c73014fa399a04e6dcc34636387797
0xevolve <Artevolve@yahoo.com>
2024-05-20 16:07:41 +0100

    feat: message format

--- rust/tests/mailbox.rs
+++ rust/tests/mailbox.rs
@@ -3,7 +3,9 @@ mod constants;
 mod contracts;
 mod validator;
 
-use contracts::strk::mailbox::{mailbox, Bytes, Dispatch as DispatchEvent};
+use bytes::{BufMut, BytesMut};
+use cainome::cairo_serde::CairoSerde;
+use contracts::strk::mailbox::{mailbox, Bytes, Dispatch as DispatchEvent, Message};
 use ethers::{
     prelude::parse_log, providers::Middleware, signers::Signer, types::TransactionReceipt,
 };
@@ -29,7 +31,60 @@ pub fn parse_dispatch_from_res(events: &[Event]) -> DispatchEvent {
         sender: cainome::cairo_serde::ContractAddress(found.data[0]),
         destination_domain: found.data[1].try_into().unwrap(),
         recipient_address: cainome::cairo_serde::ContractAddress(found.data[2]),
-        message: (found.data[3], found.data[4]).try_into().unwrap(),
+        message: Message::cairo_deserialize(&found.data, 3).expect("Failed to deserialize message"),
+    }
+}
+
+fn u128_vec_to_u8_vec(input: Vec<u128>) -> Vec<u8> {
+    let mut output = Vec::with_capacity(input.len() * 16);
+    for value in input {
+        output.extend_from_slice(&value.to_be_bytes());
+    }
+    output
+}
+
+/// Convert a starknet message to eth message bytes
+fn to_eth_message_bytes(starknet_message: Message) -> Vec<u8> {
+    let mut buf =
+        BytesMut::with_capacity(1 + 4 + 4 + 32 + 4 + 32 + starknet_message.body.size as usize);
+
+    buf.put_u8(starknet_message.version);
+    buf.put_u32(starknet_message.nonce);
+    buf.put_u32(starknet_message.origin);
+    buf.put_slice(&starknet_message.sender.0.to_bytes_be().as_slice());
+    buf.put_u32(starknet_message.destination);
+    buf.put_slice(starknet_message.recipient.0.to_bytes_be().as_slice());
+    buf.put_slice(&u128_vec_to_u8_vec(starknet_message.body.data));
+
+    println!("ETH message bytes: {:?}", buf.to_vec());
+
+    buf.to_vec()
+}
+
+/// Convert a byte slice to a starknet message
+/// We have to pad the bytes to 16 bytes chunks
+/// see here for more info https://github.com/keep-starknet-strange/alexandria/blob/main/src/bytes/src/bytes.cairo#L16
+fn to_strk_message_bytes(bytes: &[u8]) -> Bytes {
+    // Calculate the required padding
+    let padding = (16 - (bytes.len() % 16)) % 16;
+    let total_len = bytes.len() + padding;
+
+    // Create a new byte vector with the necessary padding
+    let mut padded_bytes = Vec::with_capacity(total_len);
+    padded_bytes.extend_from_slice(bytes);
+    padded_bytes.extend(std::iter::repeat(0).take(padding));
+
+    let mut result = Vec::with_capacity(total_len / 16);
+    for chunk in padded_bytes.chunks_exact(16) {
+        // Convert each 16-byte chunk into a u128
+        let mut array = [0u8; 16];
+        array.copy_from_slice(chunk);
+        result.push(u128::from_be_bytes(array));
+    }
+
+    Bytes {
+        size: bytes.len() as u32,
+        data: result,
     }
 }
 
@@ -52,10 +107,7 @@ where
         .dispatch(
             &DOMAIN_EVM,
             &cainome::cairo_serde::ContractAddress(FieldElement::from_bytes_be(&receiver).unwrap()),
-            &Bytes {
-                size: msg_body.len() as u32,
-                data: msg_body.iter().map(|b| *b as u128).collect(),
-            },
+            &to_strk_message_bytes(msg_body),
             &None,
             &None,
         )
@@ -83,17 +135,17 @@ where
 
     println!("\nDispatched: {:?}", dispatch);
 
-    let process_tx = to.core.mailbox.process(
-        vec![].into(),
-        dispatch.message.to_bytes_be().as_slice().to_vec().into(),
-    );
+    let process_tx = to
+        .core
+        .mailbox
+        .process(vec![].into(), to_eth_message_bytes(dispatch.message).into());
     let process_tx_res = process_tx.send().await?.await?.unwrap();
 
     Ok(process_tx_res)
 }
 
 #[tokio::test]
-async fn test_mailbox_cw_to_evm() -> eyre::Result<()> {
+async fn test_mailbox_strk_to_evm() -> eyre::Result<()> {
     // init starknet env
     let strk = strk::setup_env(DOMAIN_STRK, &[TestValidators::new(DOMAIN_EVM, 5, 3)]).await?;
 
